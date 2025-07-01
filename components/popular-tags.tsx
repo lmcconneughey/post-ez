6fea51e88fbf5b18cb1f252292d73c7c946cfe96
Lawrence McConneughey <lmcconneughey@gmail.com>
2025-07-01 15:51:19 -0400

    style: test data/images to right bar

--- components/popular-tags.tsx
+++ components/popular-tags.tsx
@@ -1,5 +1,90 @@
+import { Ellipsis } from "lucide-react";
+import ImageComponent from "./image";
+import Link from "next/link";
+
 const PopularTags = () => {
-    return ( <>Popular Tags</> );
+    return ( 
+        <div className='p-4 rounded-2xl border-[1px] border-borderGray flex flex-col gap-4'>
+            <h1 className="text-xl font-bold text-textGraylight">{"What's"} Happening </h1>
+            {/* trend */}
+            <div className="flex gap-4">
+                <div className="relative w-20 h-20 round-xl overflow-hidden">
+                    {/* image */}
+                    <ImageComponent 
+                        path='posts/flute-7296582_1280.jpg'
+                        alt={'event image'}
+                        w={120}
+                        h={120}
+                        tr={true} 
+                    />
+                </div>
+                <div className="flex-1">
+                    <h2 className="text-textGraylight font-bold">The jazz club!</h2>
+                    <span className="text-sm text-textGray">Last Night</span>
+                </div>
+            </div>
+            {/* topics */}
+            <div className="">
+                <div className="flex items-center justify-between">
+                    <span className="text-textGray text-sm">Technology * Trending</span>
+                        <Ellipsis 
+                            strokeWidth={1}
+                        />
+                </div>                
+                    <h2 className="text-textGraylight font-bold">OpenAI</h2>
+                    <span className="text-textGray text-sm">20k posts</span>
+            </div>
+            {/* topics */}
+            <div className="">
+                <div className="flex items-center justify-between">
+                    <span className="text-textGray text-sm">Technology * Trending</span>
+                        <Ellipsis 
+                            strokeWidth={1}
+                        />
+                </div>                
+                    <h2 className="text-textGraylight font-bold">OpenAI</h2>
+                    <span className="text-textGray text-sm">20k posts</span>
+            </div>
+            {/* topics */}
+            <div className="">
+                <div className="flex items-center justify-between">
+                    <span className="text-textGray text-sm">Technology * Trending</span>
+                        <Ellipsis 
+                            strokeWidth={1}
+                        />
+                </div>                
+                    <h2 className="text-textGraylight font-bold">OpenAI</h2>
+                    <span className="text-textGray text-sm">20k posts</span>
+            </div>
+            {/* topics */}
+            <div className="">
+                <div className="flex items-center justify-between">
+                    <span className="text-textGray text-sm">Technology * Trending</span>
+                        <Ellipsis 
+                            strokeWidth={1}
+                        />
+                </div>                
+                    <h2 className="text-textGraylight font-bold">OpenAI</h2>
+                    <span className="text-textGray text-sm">20k posts</span>
+            </div>
+            {/* topics */}
+            <div className="">
+                <div className="flex items-center justify-between">
+                    <span className="text-textGray text-sm">Technology * Trending</span>
+                        <Ellipsis 
+                            strokeWidth={1}
+                        />
+                </div>                
+                    <h2 className="text-textGraylight font-bold">OpenAI</h2>
+                    <span className="text-textGray text-sm">20k posts</span>
+            </div>
+            <Link 
+                className="text-iconBlue"
+                href='/'
+            >
+                Show More
+            </Link>
+        </div> );
 }
  
 export default PopularTags;
--- components/recommendations.tsx
+++ components/recommendations.tsx
@@ -1,5 +1,80 @@
+import Link from "next/link";
+import ImageComponent from "./image";
+
 const Recommendations = () => {
-    return ( <>Recommendations</> );
+    return ( 
+        <div className="p-4 rounded-2xl border-[1px] border-borderGray flex flex-col gap-4">
+            {/* user card */}
+            <div className="flex items-center justify-between">
+                <div className="flex items-center gap-2">
+                    <div className="relative rounded-full overflow-hidden w-10 h-10">
+                        <ImageComponent 
+                            path='posts/woman-6401957_640.jpg'
+                            alt='test avatar Doe John'
+                            w={100}
+                            h={100}
+                            tr={true}
+                        />
+                    </div>  
+                    <div className="">
+                        <h1 className="text-md font-bold">Jane Doe</h1>
+                        <span className="text-textGray text-sm">@JaneDoe</span>
+                    </div>  
+                </div>
+                {/* button */}
+                <button className="py-1 px-4 font-semibold bg-white text-black rounded-full">
+                    Follow
+                </button>
+            </div>
+            <div className="flex items-center justify-between">
+                <div className="flex items-center gap-2">
+                    <div className="relative rounded-full overflow-hidden w-10 h-10">
+                        <ImageComponent 
+                            path='posts/woman-6401957_640.jpg'
+                            alt='test avatar Doe John'
+                            w={100}
+                            h={100}
+                            tr={true}
+                        />
+                    </div>  
+                    <div className="">
+                        <h1 className="text-md font-bold">Jane Doe</h1>
+                        <span className="text-textGray text-sm">@JaneDoe</span>
+                    </div>  
+                </div>
+                {/* button */}
+                <button className="py-1 px-4 font-semibold bg-white text-black rounded-full">
+                    Follow
+                </button>
+            </div>
+            <div className="flex items-center justify-between">
+                <div className="flex items-center gap-2">
+                    <div className="relative rounded-full overflow-hidden w-10 h-10">
+                        <ImageComponent 
+                            path='posts/woman-6401957_640.jpg'
+                            alt='test avatar Doe John'
+                            w={100}
+                            h={100}
+                            tr={true}
+                        />
+                    </div>  
+                    <div className="">
+                        <h1 className="text-md font-bold">Jane Doe</h1>
+                        <span className="text-textGray text-sm">@JaneDoe</span>
+                    </div>  
+                </div>
+                {/* button */}
+                <button className="py-1 px-4 font-semibold bg-white text-black rounded-full">
+                    Follow
+                </button>
+            </div>
+            <Link 
+                className="text-iconBlue"
+                href='/'
+            >
+                Show More
+            </Link>
+        </div> );
 }
  
 export default Recommendations;
