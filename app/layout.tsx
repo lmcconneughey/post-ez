42dd0766e1ccf7c626c4b9a6e7eb0c7f30e36630
lawrence mcconneughey <lmcconneughey@gmail.com>
2025-07-03 13:23:27 -0400

    add: parallel routing for post dialog window

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,20 +4,23 @@ import RightBar from '../components/right-bar';
 
 export default function RootLayout({
     children,
+    modal,
 }: Readonly<{
     children: React.ReactNode;
+    modal: React.ReactNode;
 }>) {
     return (
-        <html lang="en">
+        <html lang='en'>
             <body>
-                <div className="max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl 2xl:max-w-screen-2xl mx-auto flex justify-between">
-                    <div className="px-2 xsm:px-4 2xl:px-8 ">
+                <div className='max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl 2xl:max-w-screen-2xl mx-auto flex justify-between'>
+                    <div className='px-2 xsm:px-4 2xl:px-8 '>
                         <LeftBar />
                     </div>
-                    <div className="flex-1 lg:min-w-[600px] border-x-[1px] border-borderGray">
+                    <div className='flex-1 lg:min-w-[600px] border-x-[1px] border-borderGray'>
                         {children}
+                        {modal}
                     </div>
-                    <div className="hidden lg:flex ml-4 md:ml-8 flex-1">
+                    <div className='hidden lg:flex ml-4 md:ml-8 flex-1'>
                         <RightBar />
                     </div>
                 </div>
--- app/page.tsx
+++ app/page.tsx
@@ -4,44 +4,32 @@ import Share from '../components/share';
 import Link from 'next/link';
 
 const Homepage = () => {
-  return (
-      <div className=''>
-          <div className="px-4 pt-4 flex justify-between text-textGray font-bold border-b-[1px] border-borderGray">
-              <Link 
-                  className='pb-3 flex items-center border-b-4 border-iconBlue'
-                  href='/'
-              >
-                  For you
-              </Link>
-              <Link 
-                  className='pb-3 flex items-center'
-                  href='/'
-              >
-                  Following
-              </Link>
-              <Link 
-                  className='hidden pb-3 md:flex items-center'
-                  href='/'
-              >
-                  Victor Wooten
-              </Link>
-              <Link 
-                  className='hidden pb-3 md:flex items-center'
-                  href='/'
-              >
-                  Mohini Dey
-              </Link>
-              <Link 
-                  className='hidden pb-3 md:flex items-center'
-                  href='/'
-              >
-                  Esperanza Spalding
-              </Link>
-          </div>
-          <Share />
-          <Feed />
-      </div>
-  )
-}
+    return (
+        <div className=''>
+            <div className='px-4 pt-4 flex justify-between text-textGray font-bold border-b-[1px] border-borderGray'>
+                <Link
+                    className='pb-3 flex items-center border-b-4 border-iconBlue'
+                    href='/'
+                >
+                    For you
+                </Link>
+                <Link className='pb-3 flex items-center' href='/'>
+                    Following
+                </Link>
+                <Link className='hidden pb-3 md:flex items-center' href='/'>
+                    Victor Wooten
+                </Link>
+                <Link className='hidden pb-3 md:flex items-center' href='/'>
+                    Mohini Dey
+                </Link>
+                <Link className='hidden pb-3 md:flex items-center' href='/'>
+                    Esperanza Spalding
+                </Link>
+            </div>
+            <Share />
+            <Feed />
+        </div>
+    );
+};
 
-export default Homepage
+export default Homepage;
