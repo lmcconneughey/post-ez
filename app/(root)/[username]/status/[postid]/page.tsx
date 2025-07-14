1f5c78b2ab616ea7cb702363a5fd2a566558cb2f
lawrence mcconneughey <lmcconneughey@gmail.com>
2025-07-14 00:44:09 -0400

    add: user details populate StatusPage from db

--- app/(root)/[username]/status/[postid]/page.tsx
+++ app/(root)/[username]/status/[postid]/page.tsx
@@ -2,8 +2,112 @@ import { ArrowLeftIcon } from 'lucide-react';
 import Link from 'next/link';
 import Post from '../../../../../components/post';
 import Comments from '../../../../../components/comments';
+import { prisma } from '../../../../../db/prisma';
+import { auth } from '@clerk/nextjs/server';
+import { notFound } from 'next/navigation';
 
-const StatusPage = () => {
+const StatusPage = async ({
+    params,
+}: {
+    params: Promise<{ username: string; postId: string }>;
+}) => {
+    const { userId } = await auth();
+
+    if (!userId) return;
+
+    const postId = (await params).postId;
+
+    const post = await prisma.post.findFirst({
+        where: {
+            id: postId,
+        },
+        include: {
+            user: {
+                select: {
+                    displayName: true,
+                    userName: true,
+                    img: true,
+                },
+            },
+
+            _count: {
+                select: {
+                    Like: true,
+                    reposts: true,
+                    comments: true,
+                },
+            },
+            Like: {
+                where: {
+                    userId, // userId : current userId
+                },
+                select: {
+                    id: true, // if the id exists, user already liked post
+                },
+            },
+            reposts: {
+                where: {
+                    userId, // userId : current userId
+                },
+                select: {
+                    id: true, // if the id exists, user already liked post
+                },
+            },
+            SavedPost: {
+                where: {
+                    userId, // userId : current userId
+                },
+                select: {
+                    id: true, // if the id exists, user already liked post
+                },
+            },
+            comments: {
+                include: {
+                    user: {
+                        select: {
+                            displayName: true,
+                            userName: true,
+                            img: true,
+                        },
+                    },
+
+                    _count: {
+                        select: {
+                            Like: true,
+                            reposts: true,
+                            comments: true,
+                        },
+                    },
+                    Like: {
+                        where: {
+                            userId, // userId : current userId
+                        },
+                        select: {
+                            id: true, // if the id exists, user already liked post
+                        },
+                    },
+                    reposts: {
+                        where: {
+                            userId, // userId : current userId
+                        },
+                        select: {
+                            id: true, // if the id exists, user already liked post
+                        },
+                    },
+                    SavedPost: {
+                        where: {
+                            userId, // userId : current userId
+                        },
+                        select: {
+                            id: true, // if the id exists, user already liked post
+                        },
+                    },
+                },
+            },
+        },
+    });
+
+    if (!post) return notFound();
     return (
         <div className=''>
             <div className='flex items-center gap-8 sticky top-0 backdrop-blur-md p-4 z-10 bg-[#000000 84]'>
@@ -12,8 +116,12 @@ const StatusPage = () => {
                 </Link>
                 <h1 className='font-bold text-lg'>Post</h1>
             </div>
-            <Post type='status' />
-            <Comments />
+            <Post type='status' post={post} />
+            <Comments
+                comments={post.comments}
+                postId={post.id}
+                username={post.user.userName}
+            />
         </div>
     );
 };
