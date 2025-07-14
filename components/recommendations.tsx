096b4c5cb879c3a15ff836b2e7e594eaf7e94ebc
lawrence mcconneughey <lmcconneughey@gmail.com>
2025-07-14 00:55:02 -0400

    add: recommend users that the current user is not already following

--- components/recommendations.tsx
+++ components/recommendations.tsx
@@ -1,80 +1,91 @@
-import Link from "next/link";
-import ImageComponent from "./image";
+import Link from 'next/link';
+import ImageComponent from './image';
+import { prisma } from '../db/prisma';
+import { auth } from '@clerk/nextjs/server';
 
-const Recommendations = () => {
-    return ( 
-        <div className="p-4 rounded-2xl border-[1px] border-borderGray flex flex-col gap-4">
-            {/* user card */}
-            <div className="flex items-center justify-between">
-                <div className="flex items-center gap-2">
-                    <div className="relative rounded-full overflow-hidden w-10 h-10">
-                        <ImageComponent 
-                            path='posts/woman-6401957_640.jpg'
-                            alt='test avatar Doe John'
-                            w={100}
-                            h={100}
-                            tr={true}
-                        />
-                    </div>  
-                    <div className="">
-                        <h1 className="text-md font-bold">Jane Doe</h1>
-                        <span className="text-textGray text-sm">@JaneDoe</span>
-                    </div>  
+const Recommendations = async () => {
+    // authenticated user
+    const { userId } = await auth();
+
+    if (!userId) return;
+
+    const followingId = await prisma.follow.findMany({
+        where: {
+            followerId: userId,
+        },
+        select: {
+            followingId: true,
+        },
+    });
+
+    const followedUserIds = followingId.map((x) => x.followingId); // user Ids we're following
+    // Recommend users that the current user is not already following,
+    // but who are followed by the people the user is already following aka
+    // friend of friends
+    const friendRecommendations = await prisma.user.findMany({
+        where: {
+            id: {
+                not: userId,
+                notIn: followedUserIds,
+            },
+            following: {
+                some: {
+                    followerId: {
+                        in: followedUserIds,
+                    },
+                },
+            },
+        },
+        take: 3,
+        select: {
+            id: true,
+            displayName: true,
+            userName: true,
+            img: true,
+        },
+    });
+
+    return (
+        <div className='p-4 rounded-2xl border-[1px] border-borderGray flex flex-col gap-4'>
+            {friendRecommendations.map((profile) => (
+                <div
+                    className='flex items-center justify-between'
+                    key={profile.id}
+                >
+                    <div className='flex items-center gap-2'>
+                        <div className='relative rounded-full overflow-hidden w-10 h-10'>
+                            <ImageComponent
+                                path={
+                                    profile.img ||
+                                    'posts/blank-profile-picture-973460_640.png'
+                                }
+                                alt={profile.userName || 'profile pic'}
+                                w={100}
+                                h={100}
+                                tr={true}
+                            />
+                        </div>
+                        <div className=''>
+                            <h1 className='text-md font-bold'>
+                                {profile.displayName || profile.userName}
+                            </h1>
+                            <span className='text-textGray text-sm'>
+                                @{profile.userName}
+                            </span>
+                        </div>
+                    </div>
+                    {/* button */}
+                    <button className='py-1 px-4 font-semibold bg-white text-black rounded-full'>
+                        Follow
+                    </button>
                 </div>
-                {/* button */}
-                <button className="py-1 px-4 font-semibold bg-white text-black rounded-full">
-                    Follow
-                </button>
-            </div>
-            <div className="flex items-center justify-between">
-                <div className="flex items-center gap-2">
-                    <div className="relative rounded-full overflow-hidden w-10 h-10">
-                        <ImageComponent 
-                            path='posts/woman-6401957_640.jpg'
-                            alt='test avatar Doe John'
-                            w={100}
-                            h={100}
-                            tr={true}
-                        />
-                    </div>  
-                    <div className="">
-                        <h1 className="text-md font-bold">Jane Doe</h1>
-                        <span className="text-textGray text-sm">@JaneDoe</span>
-                    </div>  
-                </div>
-                {/* button */}
-                <button className="py-1 px-4 font-semibold bg-white text-black rounded-full">
-                    Follow
-                </button>
-            </div>
-            <div className="flex items-center justify-between">
-                <div className="flex items-center gap-2">
-                    <div className="relative rounded-full overflow-hidden w-10 h-10">
-                        <ImageComponent 
-                            path='posts/woman-6401957_640.jpg'
-                            alt='test avatar Doe John'
-                            w={100}
-                            h={100}
-                            tr={true}
-                        />
-                    </div>  
-                    <div className="">
-                        <h1 className="text-md font-bold">Jane Doe</h1>
-                        <span className="text-textGray text-sm">@JaneDoe</span>
-                    </div>  
-                </div>
-                {/* button */}
-                <button className="py-1 px-4 font-semibold bg-white text-black rounded-full">
-                    Follow
-                </button>
-            </div>
-            <Link 
-                className="text-iconBlue"
-                href='/'
-            >
+            ))}
+
+            <Link className='text-iconBlue' href='/'>
                 Show More
             </Link>
-        </div> );
-}
- 
+        </div>
+    );
+};
+
 export default Recommendations;
