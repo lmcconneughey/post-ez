9e0bdf367c416594baab4b19c38183b8757a9c0d
lawrence mcconneughey <lmcconneughey@gmail.com>
2025-07-23 15:44:46 -0400

    add: detailed logs to help debug

--- lib/socket.ts
+++ lib/socket.ts
@@ -9,7 +9,17 @@ export const connectSocket = (): Socket => {
         const WS_URL =
             process.env.NEXT_PUBLIC_WS_URL || 'http://localhost:3001';
         socket = io(WS_URL, {
-            transports: ['websocket'],
+            // transports: ['websocket'],
+        });
+        socket.on('connect', () => {
+            console.log('CLIENT: Socket.IO connected successfully!');
+        });
+        socket.on('disconnect', (reason) => {
+            console.log('CLIENT: Socket.IO disconnected. Reason:', reason);
+        });
+        socket.on('connect_error', (error) => {
+            console.error('CLIENT: Socket.IO connection error:', error.message);
+            // console.error('CLIENT: Socket.IO connection error details:', error.data);
         });
     }
     return socket;
