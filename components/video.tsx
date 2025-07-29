9370a719c0c58dd14e39a8417d9d90e1ef294b86
lawrence mcconneughey <lmcconneughey@gmail.com>
2025-07-29 13:58:31 -0400

    add: video previews when video is in view window

--- components/video.tsx
+++ components/video.tsx
@@ -1,27 +1,26 @@
-'use client'
+'use client';
 
-import { Video } from "@imagekit/next";
+import { Video } from '@imagekit/next';
 
-const urlEndpoint= process.env.NEXT_PUBLIC_URL_ENDPOINT
+const urlEndpoint = process.env.NEXT_PUBLIC_URL_ENDPOINT;
 
 type VideoTypes = {
     path: string;
     classname?: string;
-}
+};
 
-const VideoComponent = ({path, classname}: VideoTypes) => {
-    
+const VideoComponent = ({ path, classname }: VideoTypes) => {
     return (
-            <Video
-                urlEndpoint={urlEndpoint}
-                src={path} // The path to the video in your ImageKit account
-                classname={classname}
-                transformation={[{ height: 400, width: 600, quality: 90 }]}
-                controls 
-            />
-    )
-}
-       
-        
- 
+        <Video
+            autoPlay
+            muted
+            urlEndpoint={urlEndpoint}
+            src={path} // The path to the video in your ImageKit account
+            classname={classname}
+            transformation={[{ height: 400, width: 600, quality: 90 }]}
+            controls
+        />
+    );
+};
+
 export default VideoComponent;
