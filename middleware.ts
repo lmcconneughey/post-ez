2d8a465bc1798eaa31b19896b38b13a82cff2109
lawrence mcconneughey <lmcconneughey@gmail.com>
2025-07-31 14:25:01 -0400

    chore: refactor middleware for global protection

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,12 @@
 import { clerkMiddleware } from '@clerk/nextjs/server';
 
-//const isProtectedRoute = createRouteMatcher('/');
-
 export default clerkMiddleware(
-    async (auth) => {
+    async (auth, req) => {
+        const url = req.nextUrl.pathname;
+
+        const publicRoutes = ['/sign-in', '/sign-up'];
+        if (publicRoutes.includes(url)) return;
+
         await auth.protect();
     },
     {
@@ -14,9 +17,8 @@ export default clerkMiddleware(
 
 export const config = {
     matcher: [
-        // Skip Next.js internals and all static files, unless found in search params
         '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-        // Always run for API routes
+
         '/(api|trpc)(.*)',
     ],
 };
