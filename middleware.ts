45b35324d67c17589226a86d9ead8384ab717ffe
lawrence mcconneughey <lmcconneughey@gmail.com>
2025-08-01 12:20:38 -0400

    refactor: update middleware for deployment

--- middleware.ts
+++ middleware.ts
@@ -16,9 +16,5 @@ export default clerkMiddleware(
 );
 
 export const config = {
-    matcher: [
-        '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-
-        '/(api|trpc)(.*)',
-    ],
+    matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
 };
