117f5124cdaba5e7131f187d4193fab724b54012
lawrence mcconneughey <lmcconneughey@gmail.com>
2025-08-04 16:40:03 -0400

    add: dynamic link to recommended user and follow button is now functional

--- components/recommendations.tsx
+++ components/recommendations.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import ImageComponent from './image';
 import { prisma } from '../db/prisma';
 import { auth } from '@clerk/nextjs/server';
+import FollowButton from './follow-button';
 
 const Recommendations = async () => {
     // authenticated user
@@ -45,39 +46,57 @@ const Recommendations = async () => {
         },
     });
 
+    const followStatuses = await prisma.follow
+        .findMany({
+            where: {
+                followerId: userId,
+                followingId: {
+                    in: friendRecommendations.map((rec) => rec.id),
+                },
+            },
+            select: {
+                followingId: true,
+            },
+        })
+        .then((statuses) => statuses.map((s) => s.followingId));
+
     return (
         <div className='p-4 rounded-2xl border-[1px] border-borderGray flex flex-col gap-4'>
             {friendRecommendations.map((profile) => (
                 <div
-                    className='flex items-center justify-between'
+                    className='flex items-center justify-between cursor-pointer'
                     key={profile.id}
                 >
-                    <div className='flex items-center gap-2'>
-                        <div className='relative rounded-full overflow-hidden w-10 h-10'>
-                            <ImageComponent
-                                path={
-                                    profile.img ||
-                                    'posts/blank-profile-picture-973460_640.png'
-                                }
-                                alt={profile.userName || 'profile pic'}
-                                w={100}
-                                h={100}
-                                tr={true}
-                            />
+                    <Link href={`/${profile.userName}`}>
+                        <div className='flex items-center gap-2'>
+                            <div className='relative rounded-full overflow-hidden w-10 h-10'>
+                                <ImageComponent
+                                    path={
+                                        profile.img ||
+                                        'posts/blank-profile-picture-973460_640.png'
+                                    }
+                                    alt={profile.userName || 'profile pic'}
+                                    w={100}
+                                    h={100}
+                                    tr={true}
+                                />
+                            </div>
+                            <div className=''>
+                                <h1 className='text-md font-bold'>
+                                    {profile.displayName || profile.userName}
+                                </h1>
+                                <span className='text-textGray text-sm'>
+                                    @{profile.userName}
+                                </span>
+                            </div>
                         </div>
-                        <div className=''>
-                            <h1 className='text-md font-bold'>
-                                {profile.displayName || profile.userName}
-                            </h1>
-                            <span className='text-textGray text-sm'>
-                                @{profile.userName}
-                            </span>
-                        </div>
-                    </div>
+                    </Link>
                     {/* button */}
-                    <button className='py-1 px-4 font-semibold bg-white text-black rounded-full'>
-                        Follow
-                    </button>
+
+                    <FollowButton
+                        userId={profile.id}
+                        isFollowed={followStatuses.includes(profile.id)}
+                    />
                 </div>
             ))}
 
