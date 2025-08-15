3e032b037fcab729b7f5a702333d1aaf41640d23
lawrence mcconneughey <lmcconneughey@gmail.com>
2025-08-15 17:00:27 -0400

    chore: edit modal testing

--- app/(root)/[username]/@edit-modal/page.tsx
+++ app/(root)/[username]/@edit-modal/page.tsx
@@ -1,8 +1,29 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+
 const EditModal = () => {
+    const router = useRouter();
+
+    const handleClose = () => {
+        router.back();
+    };
+
     return (
-        <form className='fixed w-screen h-screen top-0 left-0 z-[9999] bg-[#293139a6] flex justify-center'>
-            <div className=''>Hello World</div>
-        </form>
+        <div
+            onClick={handleClose}
+            className='fixed w-screen h-screen top-0 left-0 z-[9999] bg-[#293139a6] flex justify-center items-center'
+        >
+            <div
+                onClick={(e) => e.stopPropagation()}
+                className='bg-gray-800 p-8 rounded-lg shadow-lg'
+            >
+                <form>
+                    <div className='text-white text-2xl'>Hello World</div>
+                </form>
+                <button onClick={handleClose}>Close Modal</button>
+            </div>
+        </div>
     );
 };
 
